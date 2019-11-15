a7f81382ccabafa74ea30e7ef57526bd10be17c1
umr-33 <xx_yr@icloud.com>
2019-11-15 20:29:02 +0900

    view編集

--- app/controllers/stays_controller.rb
+++ app/controllers/stays_controller.rb
@@ -1,66 +1,8 @@
 class StaysController < ApplicationController
   def index
-    def index
-
-      t = Time.new
-      plan = Plan.find(params[:plan_id].to_i)
-      room = Room.find(params[:room_id].to_i)
-  
-      case room.basic_info.reserve_period
-      when "三ヶ月先" then
-        set_sime = 3
-      when "六ヶ月先" then
-        set_sime = 6
-      when "九ヶ月先" then
-        set_sime = 9
-      when "十二ヶ月先" then
-        set_sime = 12
-      else
-        set_sime = 0
-      end
-  
-      year = t.year
-      sime = t.since(set_sime.month).strftime("20%y-%m-%d")
-      today = t.strftime("20%y-%m-%d")
-  
-      @dates = (Date.parse(today)..Date.parse(sime)) ###当日から予約締め切り日までの日付を全取得
-  
-      @reserves = Reserve.where(room_id: params[:room_id].to_i) #対象施設の予約情報を取得
-      reserve_days = []
-      @reserves.each do |re|
-        re.reserve_dates.each do |day|
-          reserve_days << day.start_date    
-        end
-      end
-  
-      cant_weeks = plan.weeks.map{|w|w.name if w.can == false }.compact  #対象施設の貸出不可曜日の取得
-      wd = ["祝日", "日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
-  
-      if params[:day].to_i == 0 ##時間貸しは未実装
-        @can_re = []
-        @can_re << {start: t, title: "時間貸し", class_name: "can_reserve"}
-      else ##日がしの場合
-        @can_re =[] #成形後の日付をここに入れる
-        @dates.each do |day|
-          title = "¥#{plan.day_price}~"
-          class_name = "can_reserve"
-          if reserve_days.include?(day.strftime("20%y-%m-%d")) ##予約被り
-            class_name = "cant_reserve"
-            title = "予約あり"
-          end
-  
-          if cant_weeks.include?(wd[day.strftime("%u").to_i]) ##予約不可日
-            class_name = "cant_reserve"
-            title = "不可"
-          end
-          @can_re << {start: day, class_name: class_name ,title: title  }
-        end
-      end
-  
-      respond_to do |format|
-        format.json
-      end
   end
   
+  respond_to do |format|
+    format.json
   end
 end
